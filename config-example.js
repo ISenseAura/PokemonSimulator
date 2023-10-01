4f30b3784744e88dbcdd3346351d1ccc534b141f
Mayur Hiwale (P9) <33085835+Zerapium@users.noreply.github.com>
2023-10-01 11:50:28 +0530

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -1,13 +1,7 @@
 exports.firebaseConfig = {
-    apiKey: "AIzaSyDflq9NO9m1Z3WHNI2GnLGbKsCr4QDVm2E",
-    authDomain: "ps-server-ef85b.firebaseapp.com",
-    projectId: "ps-server-ef85b",
-    storageBucket: "ps-server-ef85b.appspot.com",
-    messagingSenderId: "703224640689",
-    appId: "1:703224640689:web:ed188c0a414db979a71e90",
-    measurementId: "G-TSPEY2VDNJ"
+//your config
   };
 
-  exports.jwtKey = "mayur";
+  exports.jwtKey = "your";
 
-  exports.salt = 10;
+  exports.salt = 1000000;
